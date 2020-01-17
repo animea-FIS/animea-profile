d9008c84cc16e6659302be8bd02db5bf6361cfe1
arodriguez <arodriguez@bebeyond.es>
2020-01-17 11:25:44 +0000

    Swagger definition implemented at /api-docs

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 console.log("Lanzando servidor de API de perfil...");
 
 const app = require('../server.js');
+const swaggerUi = require('express-swaggerize-ui');
 const PORT = process.env.PORT || 3005;
 
 app.listen(PORT, () => console.info(`Servidor en funcionamiento en el puerto ${PORT}`));
